0c6a94240433931b3f62dacb6db0e38bb972a1fa
Santiago Cano <2704@holbertonschool.com>
2021-02-03 13:41:30 -0500

    spell unsigned properly

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@ long int li;
 long long int lli;
 float f;
 
-printf("Size of a char: %c byte(s)\n", (unisgned long)sizeof(c));
-printf("Size of an int: %i byte(s)\n", (unisgned long)sizeof(i));
-printf("Size of a long int: %li byte(s)\n", (unisgned long)sizeof(li));
-printf("Size of a long long int: %lli bite(s)\n", (unisgned long)sizeof(lli));
-printf("Size of a float: %f byte(s)\n", (unisgned long)sizeof(f));
+printf("Size of a char: %c byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of an int: %i byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a long int: %li byte(s)\n", (unsigned long)sizeof(li));
+printf("Size of a long long int: %lli bite(s)\n", (unsigned long)sizeof(lli));
+printf("Size of a float: %f byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
