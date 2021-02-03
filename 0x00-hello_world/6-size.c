9e2978607d6ff41c1b9bd465c51736e274fe12ea
Santiago Cano <2704@holbertonschool.com>
2021-02-03 16:25:24 -0500

    change ld to lo

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-printf("Size of a char: %ld byte(s)\n", sizeof(char));
-printf("Size of an int: %ld byte(s)\n", sizeof(int));
-printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %ld bite(s)\n", sizeof(long long int));
-printf("Size of a float: %ld byte(s)\n", sizeof(float));
+printf("Size of a char: %lo byte(s)\n", sizeof(char));
+printf("Size of an int: %lo byte(s)\n", sizeof(int));
+printf("Size of a long int: %lo byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lo bite(s)\n", sizeof(long long int));
+printf("Size of a float: %lo byte(s)\n", sizeof(float));
 return (0);
 }
