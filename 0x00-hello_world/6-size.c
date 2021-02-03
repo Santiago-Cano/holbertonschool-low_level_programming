a5608aa9024e26fe479f8d38f9498463955f61a6
Santiago Cano <2704@holbertonschool.com>
2021-02-03 13:45:20 -0500

    replace variables with types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,16 +2,10 @@
 
 int main(void)
 {
-char c;
-int i;
-long int li;
-long long int lli;
-float f;
-
-printf("Size of a char: %c byte(s)\n", sizeof(c));
-printf("Size of an int: %i byte(s)\n", sizeof(i));
-printf("Size of a long int: %li byte(s)\n", sizeof(li));
-printf("Size of a long long int: %lli bite(s)\n", sizeof(lli));
-printf("Size of a float: %f byte(s)\n", sizeof(f));
+printf("Size of a char: %c byte(s)\n", sizeof(char));
+printf("Size of an int: %i byte(s)\n", sizeof(int));
+printf("Size of a long int: %li byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lli bite(s)\n", sizeof(long long int));
+printf("Size of a float: %f byte(s)\n", sizeof(float));
 return (0);
 }
