c3fc90820647e61356fe4da490ddaa336106445b
Santiago Cano <2704@holbertonschool.com>
2021-02-04 14:19:10 -0500

    first commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print base 16 single digits starting from 0
+ * Return: 0
+ */
+
+int main(void)
+{
+char hexa;
+for (hexa = '0'; hexa <= '9'; hexa++)
+{
+putchar(hexa);
+}
+ for (hexa = 'a'; hexa <= 'f'; hexa++)
+{
+putchar(hexa);
+}
+putchar('\n');
+return (0);
+}
