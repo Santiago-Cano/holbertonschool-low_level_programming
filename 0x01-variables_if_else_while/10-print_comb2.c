cb522f483e9e6fca173332d9a81bcca3bc4ecdae
Santiago Cano <2704@holbertonschool.com>
2021-02-04 15:07:45 -0500

    remove comma at the end :)

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers from 00 to 99
+ * Return: 0
+ */
+
+int main(void)
+{
+  int digit1;
+  int digit2;
+  for (digit1 = '0'; digit1 <='9'; digit1++)
+    for (digit2 = '0'; digit2 <='9'; digit2++)
+    {
+    putchar(digit1);
+    putchar(digit2);
+    if (!(digit1 == '9' && digit2 == '9'))
+      {
+    putchar(',');
+    putchar(' ');
+      }
+    }
+  putchar('\n');
+return (0);
+}
