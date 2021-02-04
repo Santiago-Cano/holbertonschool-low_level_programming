11e1d849a9e9eab4fb7483e4232cb03d9b8c4b5c
Santiago Cano <2704@holbertonschool.com>
2021-02-04 17:27:56 -0500

    i have no clue how i made this

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - print combinations of two and two digits
+ * Return: 0
+ */
+
+int main(void)
+{
+	int digit1;
+	int digit2;
+	int digit3;
+	int digit4;
+	for (digit1 = '0'; digit1 <= '9'; digit1++)
+		for (digit2 = '0'; digit2 <= '8'; digit2++)
+			for (digit3 = (digit1); digit3 <= '9'; digit3++)
+				for (digit4 = '0'; digit4 <= '9'; digit4++)
+				{
+					if (digit3 == digit1 && digit4 == digit2)
+					{
+						digit4 = digit4 + 1;
+					}
+						putchar(digit1);
+						putchar(digit2);
+						putchar(' ');
+						putchar(digit3);
+						putchar(digit4);
+						if (!(digit1 == '9' && digit2 == '8' && digit3 == '9' && digit4 == '9'))
+						{
+							putchar(',');
+							putchar(' ');
+						}
+				}
+
+	putchar('\n');
+	return (0);
+}
