db39162cd976e1287bc0228c07bf7f7324f87c8b
Santiago Cano <2704@holbertonschool.com>
2021-02-04 15:11:32 -0500

    remove spaces

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -7,19 +7,19 @@
 
 int main(void)
 {
-  int digit1;
-  int digit2;
-  for (digit1 = '0'; digit1 <='9'; digit1++)
-    for (digit2 = '0'; digit2 <='9'; digit2++)
-    {
-    putchar(digit1);
-    putchar(digit2);
-    if (!(digit1 == '9' && digit2 == '9'))
-      {
-    putchar(',');
-    putchar(' ');
-      }
-    }
-  putchar('\n');
+int digit1;
+int digit2;
+for (digit1 = '0'; digit1 <= '9'; digit1++)
+for (digit2 = '0'; digit2 <= '9'; digit2++)
+{
+putchar(digit1);
+putchar(digit2);
+if (!(digit1 == '9' && digit2 == '9'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
 return (0);
 }
