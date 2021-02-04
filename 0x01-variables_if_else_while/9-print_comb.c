4e0441dfe8b6978be60feb0a4d55980768cc7ddd
Santiago Cano <2704@holbertonschool.com>
2021-02-04 15:17:30 -0500

    fix double parenthesis

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers from 00 to 99
+ * Return: 0
+ */
+
+int main(void)
+{
+int digit1;
+int digit2;
+for (digit1 = '0'; digit1 <= '9'; digit1++)
+{
+putchar(digit1);
+ if (!(digit1 == '9'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
