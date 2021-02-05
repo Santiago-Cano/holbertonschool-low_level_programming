dd012f17784754cb6d576343f03fa23d1780721d
Santiago Cano <2704@holbertonschool.com>
2021-02-04 19:13:53 -0500

    use if at 3rd digit, dont initialize digit4 on for function

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,26 +11,34 @@ int main(void)
 	int digit2;
 	int digit3;
 	int digit4;
+
 	for (digit1 = '0'; digit1 <= '9'; digit1++)
-		for (digit2 = '0'; digit2 <= '8'; digit2++)
+	{
+		for (digit2 = '0'; digit2 <= '9'; digit2++)
+		{
 			for (digit3 = (digit1); digit3 <= '9'; digit3++)
-				for (digit4 = '0'; digit4 <= '9'; digit4++)
+			{
+				if (digit3 == digit1)
+					digit4 = digit2 + 1;
+				else
+					digit4 = '0';
+				for (; digit4 <= '9'; digit4++)
 				{
-					if (digit3 == digit1 && digit4 == digit2)
+
+					putchar(digit1);
+					putchar(digit2);
+					putchar(' ');
+					putchar(digit3);
+					putchar(digit4);
+					if (!(digit1 == '9' && digit2 == '8' && digit3 == '9' && digit4 == '9'))
 					{
-						digit4 = digit4 + 1;
-					}
-						putchar(digit1);
-						putchar(digit2);
+						putchar(',');
 						putchar(' ');
-						putchar(digit3);
-						putchar(digit4);
-						if (!(digit1 == '9' && digit2 == '8' && digit3 == '9' && digit4 == '9'))
-						{
-							putchar(',');
-							putchar(' ');
-						}
+					}
 				}
+			}
+		}
+	}
 
 	putchar('\n');
 	return (0);
