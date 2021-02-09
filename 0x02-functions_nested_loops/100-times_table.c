b8174b3d400fa73b94417ee7a8fdfa4662d010cf
Santiago Cano <2704@holbertonschool.com>
2021-02-09 18:50:39 -0500

    account for 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,36 +11,35 @@ void print_times_table(int n)
 	int vertical;
 	int product;
 
-	if (n <= 15 && n > 0)
+	if (n <= 15 && n >= 0)
 		for (vertical = 0; vertical <= n; vertical++)
-			for (horizontal = 1; horizontal <= n; horizontal++)
+			for (horizontal = 0; horizontal <= n; horizontal++)
 			{
-				if (horizontal == 1)
-					_putchar('0');
 				product = (horizontal * vertical);
-				if (product < 10)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + product);
-				}
-				else if (product < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + (product / 10));
-					_putchar('0' + (product % 10));
-				}
+				if (horizontal == 0)
+					_putchar('0');
 				else
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar('0' + (product / 100));
-					_putchar('0' + (product / 10) % 10);
-					_putchar('0' + (product % 10));
+					if (product < 10)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar('0' + product);
+					}
+					else if (product < 100)
+					{
+						_putchar(' ');
+						_putchar('0' + (product / 10));
+						_putchar('0' + (product % 10));
+					}
+					else
+					{
+						_putchar('0' + (product / 100));
+						_putchar('0' + (product / 10) % 10);
+						_putchar('0' + (product % 10));
+					}
 				}
 				if (horizontal == n)
 					_putchar('\n');
