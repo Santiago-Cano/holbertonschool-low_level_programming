008e41a392527a09e97cc8e57fe2b09d60076a7b
Santiago Cano <2704@holbertonschool.com>
2021-02-09 09:01:45 -0500

    print 10 times instead of 11

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 	char alphabet;
 	int repeat;
 
-	for (repeat = 0; repeat <= 10; repeat++)
+	for (repeat = 0; repeat <= 9; repeat++)
 	{
 		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
