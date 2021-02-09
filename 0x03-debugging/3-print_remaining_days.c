e7329c296ad406500b96492de87d4f73feeab72d
Santiago Cano <2704@holbertonschool.com>
2021-02-09 14:51:08 -0500

    replace breaks with else

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -17,10 +17,12 @@ void print_remaining_days(int month, int day, int year)
 			if (month == 2 && day == 60)
 			{
 				printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);month = 3;
-				break;
 			}
+			else
+			{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
+			}
 		}
 		else if (!(year % 100 == 0))
 		{
@@ -34,10 +36,12 @@ void print_remaining_days(int month, int day, int year)
 			if (month == 2 && day == 60)
 			{
 				printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);month = 3;
-				break;
 			}
+			else
+			{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
+			}
 		}
 		else
 		{
