a7fc60dc267b74bfbad1f50dffd0a57d382145a4
Santiago Cano <2704@holbertonschool.com>
2021-02-09 14:47:26 -0500

    add breaks

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -15,8 +15,10 @@ void print_remaining_days(int month, int day, int year)
 	if (!(year % 4 == 0)) /* || year % 400 == 0) && !(year % 100 == 0))*/
 		{
 			if (month == 2 && day == 60)
+			{
 				printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);month = 3;
-
+				break;
+			}
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
@@ -30,8 +32,10 @@ void print_remaining_days(int month, int day, int year)
 		else if (!(year % 400 == 0))
 		{
 			if (month == 2 && day == 60)
+			{
 				printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);month = 3;
-
+				break;
+			}
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
