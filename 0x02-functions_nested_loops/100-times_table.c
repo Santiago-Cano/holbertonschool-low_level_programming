9e156dc73df6b2caf711c4eb5267e1ddeaaffbc1
Santiago Cano <2704@holbertonschool.com>
2021-02-09 15:42:57 -0500

    fix indentation

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,37 +13,37 @@ void print_times_table(int n)
 	int product;
 
 	if (!(n > 15 || n < 0))
-	for (vertical = 0; vertical <= n; vertical++)
-		for (horizontal = 1; horizontal <= n; horizontal++)
-		{
-			if (horizontal == 1)
-				_putchar('0');
-			product = (horizontal * vertical);
-			if (product < 10)
+		for (vertical = 0; vertical <= n; vertical++)
+			for (horizontal = 1; horizontal <= n; horizontal++)
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + product);
+				if (horizontal == 1)
+					_putchar('0');
+				product = (horizontal * vertical);
+				if (product < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + product);
+				}
+				else if (product < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + (product / 10));
+					_putchar('0' + (product % 10));
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar('0' + (product / 100));
+					_putchar('0' + (product / 10) % 10);
+					_putchar('0' + (product % 10));
+				}
+				if (horizontal == n)
+					_putchar('\n');
 			}
-			else if (product < 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + (product / 10));
-				_putchar('0' + (product % 10));
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar('0' + (product / 100));
-				_putchar('0' + (product / 10) % 10);
-				_putchar('0' + (product % 10));
-			}
-			if (horizontal == n)
-				_putchar('\n');
-		}
 }
