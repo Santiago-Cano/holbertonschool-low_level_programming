2b711e27a6b00ca1f659a768e8a5bd5e2acf31a4
Santiago Cano <2704@holbertonschool.com>
2021-02-11 12:15:55 -0500

    first commit

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - find largest prime factor of a number
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int number = 612852475143;
+	int divide = 2;
+	int largest = 0;
+
+	while (number != 1)
+	{
+		if ((number % divide) != 0)
+			divide++;
+		else
+		{
+			number /= divide;
+			if (number != 1)
+				largest = divide;
+		}
+	}
+	printf("%d\n", largest);
+	return (0);
+}
