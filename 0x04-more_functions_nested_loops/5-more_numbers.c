25dd1fed8e13aacc8d044022e716405b7714937c
Santiago Cano <2704@holbertonschool.com>
2021-02-11 13:36:27 -0500

    fix conditionals

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,27 @@
 #include "holberton.h"
 
 /**
- * print_more_numbers - print numbers from 0 to 14, a total of 10 times.
+ * more_numbers - print numbers from 0 to 14, a total of 10 times.
  * Return: Nothing
  */
 
-void print_more_numbers(void)
+void more_numbers(void)
 {
 	int i;
 	int counter;
+	int ten;
 
 	for (counter = '0'; counter <= '9'; counter++)
+	{
 		for (ten = '0'; ten <= '1'; ten++)
 			for (i = '0'; i <= '9'; i++)
 			{
-				if ((ten != '0') || (ten != '1' && i >= '4'))
-					_putchar(ten);
-
-				_putchar(i);
+				if (!(ten == '0'))
+				    if (!(ten == '1' && i > '4'))
+					    _putchar(ten);
+				if (!(ten == '1' && i > '4'))
+					_putchar(i);
 			}
-	_putchar('\n');
+		_putchar('\n');
+	}
 }
