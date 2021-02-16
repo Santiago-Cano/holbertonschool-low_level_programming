4c26cb8f0b04cb5a72c04a78fd65c47d7d937739
Santiago Cano <2704@holbertonschool.com>
2021-02-16 15:24:07 -0500

    first commit

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generate random passwords
+ * Return: 0
+ */
+
+int main(void)
+{
+	srand (time(NULL));
+	return (0);
+}
