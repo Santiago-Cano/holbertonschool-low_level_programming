c54de30bc18a54683b16c7b778c33f9aa89ac0a3
Santiago Cano <2704@holbertonschool.com>
2021-02-18 16:17:50 -0500

    include null value in arrays

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,8 +9,8 @@
 char *leet(char *str)
 {
 	int st = 0;
-	int compare[] = {'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L'};
-	int replace[] = {'4', '3', '0', '7', '1', '4', '3', '0', '7', '1'};
+	int compare[11] = {'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L'};
+	int replace[11] = {'4', '3', '0', '7', '1', '4', '3', '0', '7', '1'};
 	int leet;
 
 	for (; str[st] != '\0'; st++)
