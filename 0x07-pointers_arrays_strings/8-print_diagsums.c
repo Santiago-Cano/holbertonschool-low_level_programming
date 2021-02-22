a01a11b91ebfb0c266e2a04f9ba0a8e00eb46154
Santiago Cano <2704@holbertonschool.com>
2021-02-22 15:54:36 -0500

    use single dimension instead of two

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - sum two diagonals of a square matrix
+ * @a: array
+ * @size: size of square
+ */
+
+void print_diagsums(int *a, int size)
+{
+	long int sum = 0;
+	int vertical;
+
+	for (vertical = 0; vertical < size; vertical++)
+		sum += a[(vertical * size) + vertical];
+	printf("%li, ", sum);
+	sum = 0;
+	for (vertical = 1; vertical < size; vertical++)
+		sum += a[(vertical * size) - vertical];
+	printf("%li\n", sum);
+}
