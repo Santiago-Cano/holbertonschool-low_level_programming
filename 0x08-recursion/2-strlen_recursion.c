492b0d2c3f4406047f61b8a873e300f7f558397e
Santiago Cano <2704@holbertonschool.com>
2021-02-24 17:31:27 -0500

    fix documentation

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,7 +3,7 @@
 /**
  * _strlen_recursion - return the length of a string
  * @s: string
- * return - length of string as a number
+ * Return: length of string as a number
  */
 
 int _strlen_recursion(char *s)
