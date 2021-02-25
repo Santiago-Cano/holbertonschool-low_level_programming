489e5dcc139aad0b4e74af69bf1961080be39fc2
Santiago Cano <2704@holbertonschool.com>
2021-02-24 23:25:51 -0500

    first commit

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - check if two strings are identical. * can replace any string
+ * @s1: first string
+ * @s2: second string
+ * Return: 1 if identical, otherwise 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 2));
+	else if (*s2 != '*')
+		return (0);
+	else
+		return (wild(s1, s2, s2));
+}
+int wild(char *s1, char *s2, char *extra)
+{
+	if ((s2 + 1) == '*')
+		return (wild(s1, s2 + 1, s2 + 1));
+	else if (*s1 == '\0' && *s2 == '*')
+		return (wild(s1, s2 + 1, s2 + 1));
+	else if (*s1 != (s2 + 1) && *s2 == '*')
+		return (wild(s1 + 1, s2, s2));
+	else if (*s1 == extra && *s2 == '*')
+		return (wild(s1 + 1, s2, s2));
+	else if (*s1 == (s2 + 1) && *s2 == '*' && extra != '\0')
+	{
+		if (extra <= (s2 + 1))
+			return (wild(s1 + 1, s2, s2 + 2));
+		else
+			return (wild(s1 + 1, s2, extra + 1));
+	}
+	else if (*s1 == (s2 + 1) && *s2 == '*')
+		return (*s1 == 
+		return (0);
+}
