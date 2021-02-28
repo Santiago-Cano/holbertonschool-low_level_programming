cab5bdf015de8a8e9a7219dc63729dae471827fb
Santiago Cano <2704@holbertonschool.com>
2021-02-28 18:00:30 -0500

    first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print the program name, followed by a new line
+ * @argc: number of command line arguments
+ * @argv: array with the command line arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - argc]);
+}
