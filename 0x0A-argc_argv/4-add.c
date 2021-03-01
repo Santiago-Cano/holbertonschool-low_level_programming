2ed88df291aeb1de33c33e289f4e028850f9c944
Santiago Cano <2704@holbertonschool.com>
2021-02-28 21:01:11 -0500

    realize a second dimension exists

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "holberton.h"
 /**
  * main - add positive numbers
  * @argc: number of arguments
@@ -11,16 +11,17 @@
 int main(int argc, char *argv[])
 {
 	int sum = 0;
+	int ar = 1;
 
 	for (; argc > 1; argc--)
 	{
-		argv++;
-		if (!(atoi(*argv) >= '0' && atoi(*argv) <= '9'))
+		if (!(argv[ar][0] <= '9' && argv[ar][0] >= '0'))
 		{
 			printf("Error\n");
 			return (1);
 		}
-		sum += atoi(*argv);
+		sum += atoi(argv[ar]);
+		ar++;
 	}
 	printf("%d\n", sum);
 	return (0);
