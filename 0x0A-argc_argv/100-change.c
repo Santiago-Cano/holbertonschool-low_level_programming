4d9975ad91a347f3045a1a93e8bc5c4e21c20f28
Santiago Cano <2704@holbertonschool.com>
2021-02-28 21:54:02 -0500

    refactor, remove second function

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,45 +2,6 @@
 #include <stdlib.h>
 
 /**
- * coins - calculate number of coins needed and print them
- * @currency: value of currency argument
- */
-
-void coins(int currency)
-{
-	int coin = 0;
-
-	while (currency != 0)
-	{
-		if (currency >= 25)
-		{
-			currency -= 25;
-			coin++;
-		}
-		else if (currency >= 10)
-		{
-			currency -= 10;
-			coin++;
-		}
-		else if (currency >= 5)
-		{
-			currency -= 5;
-			coin++;
-		}
-		else if (currency >= 2)
-		{
-			currency -= 2;
-			coin++;
-		}
-		else
-		{
-			currency -= 1;
-			coin++;
-		}
-	}
-	printf("%d\n", coin);
-}
-/**
  * main - return minimum number of coins to make change
  * @argc: number of arguments
  * @argv: array with arguments
@@ -54,13 +15,23 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	else if (atoi(argv[1]) <= 0)
+	else if (argv[1][0] == '-')
 	{
 		printf("0\n");
 		return (0);
 	}
-	int currency = atoi(argv[1]);
+	int coins, currency = atoi(argv[1]);
 
-	coins(currency);
+	coins = currency / 25;
+	currency = currency % 25;
+	coins += currency / 10;
+	currency = currency % 10;
+	coins += currency / 5;
+	currency = currency % 5;
+	coins += currency / 2;
+	currency = currency % 2;
+	coins += currency / 1;
+
+	printf("%d\n", coins);
 	return (0);
 }
