35c22bd013b410100581b1a731720251fb16382e
Santiago Cano <2704@holbertonschool.com>
2021-03-02 22:13:17 -0500

    scrap

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -60,11 +60,11 @@ int numwords(char *str)
  * Return: NULL on failure or invalid input, otherwise a
  *
  */
-/* Hello You*/
 char **strtow(char *str)
 {
 	char **a;
-	int length, fre;
+	char *i;
+	int length, count;
 	int letter = 0;
 	int words = 1;
 
@@ -74,31 +74,9 @@ char **strtow(char *str)
 	a = (char **)malloc(sizeof(char) * words); /*set first dimension*/
 	if (a == NULL)
 		return (NULL);
-	words = 0;
-	for (length = 0; str[length] != '\0'; length++) /*set second dimension*/
-	{
-		if (str[length] == ' ' && length != 0)
-		{
-			a[words] = (char *)malloc(sizeof(char) * letter + 1);
-			if (a[words] == NULL)
-			{
-				for (fre = 0; fre < words; fre++)
-					free(a[fre]);
-				free(a);
-				return (NULL);
-			}
-			letter = 0;
-			words++;
-		}
-		else
-			letter++;
-		if (str[length] == ' ')
-			while (str[length] == ' ')
-				length++;
-		else
-			length++;
-	}
-	words = 0;
+	i = letters(str, words);
+	for (count = 0; count < words; count++)
+		a[count] = (char *)malloc(sizeof(char) * i[count]);
 	for (length = 0; str[length] != '\0'; length++) /*set values of array*/
 	{
 		if (str[length] == ' ' && length != 0)
