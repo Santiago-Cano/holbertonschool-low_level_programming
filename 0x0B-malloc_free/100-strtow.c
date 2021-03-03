482c2d6f020344726220eb9faae53c546b2f4828
Santiago Cano <2704@holbertonschool.com>
2021-03-02 22:06:23 -0500

    scrap

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -3,6 +3,58 @@
 #include <stdlib.h>
 
 /**
+ * letters - find number of letters per word
+ * @str: string
+ * @words: number of words
+ * Return: array with number of letters per word
+ */
+char *letters(char *str, int words)
+{
+	char *l[words];
+	int i, letter = 0;
+
+	words = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ' && i != 0)
+		{
+			*l[words] = letter;
+			letter = 0;
+			words++;
+		}
+		else
+			letter++;
+		if (str[i] == ' ')
+			while (str[i] == ' ')
+				i++;
+		else
+			i++;
+	}
+	return (*l);
+}
+/**
+ * numwords - fetch number of words on string
+ * @str: string
+ * Return: number of words
+ */
+int numwords(char *str)
+{
+	int words = 0;
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++) /*get number of words*/
+	{
+		if (str[i] == ' ' && i != 0)
+			words++;
+		if (str[i] == ' ')
+			while (str[i] == ' ')
+				i++;
+		else
+			i++;
+	}
+	return (words);
+}
+/**
  * strtow - split a string into an array of words
  * @str: string
  * Return: NULL on failure or invalid input, otherwise a
@@ -67,27 +119,4 @@ char **strtow(char *str)
 			length++;
 	}
 	return (a);
-}
-
-/**
- * numwords - fetch number of words on string
- * @str: string
- * Return: number of words
- */
-int numwords(*str)
-{
-	int words = 0;
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++) /*get number of words*/
-	{
-		if (str[i] == ' ' && i != 0)
-			words++;
-		if (str[i] == ' ')
-			while (str[i] == ' ')
-				i++;
-		else
-			i++;
-	}
-	return (words);
 }
