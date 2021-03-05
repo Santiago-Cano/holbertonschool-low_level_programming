324bbf0efc5fff578516e8748bcbdbc39c3af2da
Santiago Cano <2704@holbertonschool.com>
2021-03-05 09:40:43 -0500

    add functions

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -8,5 +8,7 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
+char *letters(char *str, int words, int length);
+int numwords(char *str, int length);
 
 #endif
