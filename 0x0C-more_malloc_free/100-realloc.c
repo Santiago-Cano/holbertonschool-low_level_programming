3be8f78a295a02d9448426070f20bf1bf85c4d30
Santiago Cano <2704@holbertonschool.com>
2021-03-04 22:09:34 -0500

    first commit

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _realloc - reallocate new memory to ptr and free old one
+ * @ptr: memory block
+ * @old_size: current size in bytes
+ * @new_size: new size in bytes
+ * Return: new pointer
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *a;
+	int counter;
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		a = malloc(new_size);
+		if (a == NULL)
+			return (NULL);
+		return (a);
+	}
+	a = malloc(new_size);
+	if (a == NULL)
+		return (NULL);
+	for (counter = 0; counter < new_size && counter < old_size; counter++)
+		((char *) a)[counter] = ((char *) ptr)[counter];
+	return (a);
+}
