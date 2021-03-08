ad0ed1ba5260ce4f90f45da3d5ab7d16580cef5c
Santiago Cano <2704@holbertonschool.com>
2021-03-08 14:47:37 -0500

    first commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef dog
+#define dog
+
+/**
+ * dog - structure with information about a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
