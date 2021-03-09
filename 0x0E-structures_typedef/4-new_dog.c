3386b43026cd1907839f9833d176d956dbc1ded1
Santiago Cano <2704@holbertonschool.com>
2021-03-08 21:27:52 -0500

    use strdup

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,32 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strdup - return a pointer to a newly allocated space in memory
+ * @str: string contained in new array
+ * Return: NULL if str == NULL, pointer to our string on success
+ */
+char *_strdup(char *str)
+{
+	char *a;
+	int fill, length = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[length] != '\0')
+		length++;
+
+	a = malloc(sizeof(char) * length + 1);
+
+	if (a == NULL)
+		return (NULL);
+
+	for (fill = 0; fill < length; fill++)
+		a[fill] = str[fill];
+
+	return (a);
+}
 /**
  * new_dog - create a new dog
  * @name: dog name
@@ -21,13 +47,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		for (length = 0; name[length] != '\0'; length++)
 			;
-		new_dog->name = malloc(sizeof(char) * length);
+		new_dog->name = _strdup(name);
 		if (new_dog->name == NULL)
 		{
 			free(new_dog);
 			return (NULL);
 		}
-		new_dog->name = strdup(name);
 	}
 	else
 		new_dog->name = NULL;
@@ -35,13 +60,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		for (length = 0; owner[length] != '\0'; length++)
 			;
-		new_dog->owner = malloc(sizeof(char) * length);
+		new_dog->owner = _strdup(owner);
 		if (new_dog->owner == NULL)
 		{
 			free(new_dog);
 			return (NULL);
 		}
-		new_dog->owner = strdup(owner);
 	}
 	else
 		new_dog->owner = NULL;
