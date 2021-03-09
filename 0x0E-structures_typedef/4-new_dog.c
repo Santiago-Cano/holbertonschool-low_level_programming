00658f7c9bb85d14f1dd3a926bca41a42bad19a3
Santiago Cano <2704@holbertonschool.com>
2021-03-08 20:33:29 -0500

    add copies of owner and name

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,22 +11,27 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 	int length;
+	char *namecpy, *ownercpy;
 
 	new_dog = malloc(sizeof(dog_t));
 	for (length = 0; name[length] != '\0'; length++)
 		;
 	new_dog->name = malloc(sizeof(char) * length);
-	if (new_dog->name == NULL)
+	namecpy = malloc(sizeof(char) * length);
+	if (new_dog->name == NULL || namecpy == NULL)
 		return (NULL);
-	new_dog->name = name;
+	namecpy = name;
+	new_dog->name = namecpy;
 	new_dog->age = age;
 	if (new_dog->age != age)
 		return (NULL);
 	for (length = 0; owner[length] != '\0'; length++)
 		;
 	new_dog->owner = malloc(sizeof(char) * length);
-	if (new_dog->owner == NULL)
+	ownercpy = malloc(sizeof(char) * length);
+	if (new_dog->owner == NULL || ownercpy == NULL)
 		return (NULL);
-	new_dog->owner = owner;
+	ownercpy = owner;
+	new_dog->owner = ownercpy;
 	return (new_dog);
 }
