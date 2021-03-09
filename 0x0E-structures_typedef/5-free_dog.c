ebf51d4189836c6acc46dcf4d5429e26ff8ef24a
Santiago Cano <2704@holbertonschool.com>
2021-03-08 20:58:30 -0500

    add stdlib

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,11 +1,12 @@
 #include "dog.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * free_dog - free dog structure
  * @d: dog structure
  */
 void free_dog(dog_t *d)
 {
-	free(*d);
+	free(d->name);
+	free(d->owner);
 }
