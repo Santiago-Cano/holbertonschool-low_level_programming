74dd54e2a7dd57ff7c337da69470c2d19157be37
Santiago Cano <2704@holbertonschool.com>
2021-03-10 14:57:46 -0500

    first commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - print a name using one of two pointers to functions
+ * @name: name
+ * @f: function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
