673139b1b6a5458bd229800341b3bc60866f88f6
Santiago Cano <2704@holbertonschool.com>
2021-03-10 22:26:32 -0500

    check if operator has extra character

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,14 +19,14 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	f = get_op_func(argv[2]);
-	if (*f == NULL)
+	if (*f == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
