b2e40a439fed9a138fa592ec2dc89dd2fc47c6e6
Santiago Cano <2704@holbertonschool.com>
2021-03-11 22:40:33 -0500

    add global start at beggining

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,18 +1,16 @@
 	global _start
+	section .data
+	msg db "Hello, Holberton", 10
+	len equ $-msg
 
 	section .text
-
 _start:
-	mov rax, 1        	; write(
-	mov rdi, 1        	;   STDOUT_FILENO,
-	mov rsi, msg      	;   "Hello, Holberton\n",
-	mov rdx, msglen   	;   sizeof("Hello, Holberton\n")
-	syscall           	; );
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, len
+	syscall
 
-	mov rax, 60       	; exit(
-	mov rdi, 0        	;   EXIT_SUCCESS
-	syscall           	; );
-
-	section .rodata
-msg:	 db "Hello, Holberton", 10
-msglen:	 equ $ - msg
+	mov rax, 60
+	mov rdi, 0
+	    syscall
