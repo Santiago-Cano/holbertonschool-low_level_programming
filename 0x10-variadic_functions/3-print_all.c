bf11d5426bb531d23fb57fa596a17c5be7892974
Santiago Cano <2704@holbertonschool.com>
2021-03-11 22:43:53 -0500

    first commit

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_all - print anything
+ * @char: string
+ * @format: string format
+ */
+void print_all(const char * const format, ...)
+{
+}
