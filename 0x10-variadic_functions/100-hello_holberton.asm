57567de1beb4a008f408875396572ab0858f0227
Santiago Cano <2704@holbertonschool.com>
2021-03-11 22:23:07 -0500

    use $ to find length

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,14 +1,14 @@
 global _main
 	section .data
 	msg db "Hello, Holberton", 10
-
+	len equ $-msg
 	section .text
 	global _start
 _start
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, msg
-	mov rdx, 17
+	mov rdx, len
 	syscall
 
 	mov rax, 60
