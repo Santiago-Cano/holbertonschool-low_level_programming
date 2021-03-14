097b02fc1528bf327d887812e7842b325e85967d
Santiago Cano <2704@holbertonschool.com>
2021-03-14 13:50:55 -0500

    add 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,7 +1,47 @@
-#include <stdio.h>
 #include "variadic_functions.h"
 #include <stdarg.h>
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * chart - print char
+ * @arg: char to print
+ */
+void chart(va_list arg)
+{
+	printf("%c", va_arg(arg, int));
+}
+/**
+ * intt - print int
+ * @arg: int to print
+ */
+void intt(va_list arg)
+{
+	printf("%i", va_arg(arg, int));
+}
+/**
+ * floatt - print float
+ * @arg: float to print
+ */
+void floatt(va_list arg)
+{
+	printf("%f", va_arg(arg, double));
+}
+/**
+ * stringt - print string
+ * @arg: string to print
+ */
+void stringt(va_list arg)
+{
+	char *check;
 
+	check = va_arg(arg, char *);
+	if (check == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", check);
+}
 /**
  * print_all - print anything
  * @char: string
@@ -9,4 +49,30 @@
  */
 void print_all(const char * const format, ...)
 {
+	int form = 0, ty;
+	va_list arg;
+	type_t type[] = {{'c', chart},
+			 {'i', intt},
+			 {'f', floatt},
+			 {'s', stringt},
+			 {'\0', '\0'}};
+
+	va_start(arg, format);
+	while (format != NULL && format[form] != '\0')
+	{
+		ty = 0;
+		while (type[ty].format != '\0')
+		{
+			if (format[form] == type[ty].format)
+			{
+				type[ty].f(arg);
+				if (format[form + 1] != '\0')
+					printf(", ");
+			}
+			ty++;
+		}
+		form++;
+	}
+	va_end(arg);
+	printf("\n");
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,25 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+
+/**
+ * type_t - struct
+ * @format: format type
+ * @f: pointer to function
+ */
+typedef struct type_t
+{
+	char format;
+	void (*f)(va_list);
+} type_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void chart(va_list arg);
+void intt(va_list arg);
+void floatt(va_list arg);
+void stringt(va_list arg);
 
 #endif
