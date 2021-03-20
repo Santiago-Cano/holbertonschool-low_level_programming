0db797f104169c82ef5c877890df10ea02e2ff7f
Santiago Cano <2704@holbertonschool.com>
2021-03-19 22:10:17 -0500

    follow betty guidelines

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,13 @@
 #include "lists.h"
 #include <string.h>
 #include <stdlib.h>
+
+/**
+ * add_node - add a new node to the beggining of list_t
+ * @head: head of input node
+ * @str: string to contain
+ * Return: pointer to new node
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newnode;
