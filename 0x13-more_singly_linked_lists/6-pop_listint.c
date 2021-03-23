e537ecd22bcb09e75de595a8596ca56df770a642
Santiago Cano <2704@holbertonschool.com>
2021-03-23 15:47:03 -0500

    add 6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * pop_listint - delete head of list
+ * @head: head of list
+ * Return: 0 if list is empty, otherwise n of head
+ */
+int pop_listint(listint_t **head)
+{
+	int headn;
+	listint_t *head2;
+
+	if (head == NULL)
+		return (0);
+
+	head2 = *head;
+	*head = head2->next;
+	headn = head2->n;
+	free(head2);
+	return (headn);
+}
