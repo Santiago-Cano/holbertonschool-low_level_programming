f8fb857f988aeff0710cefe20a0f8c76686590af
Santiago Cano <2704@holbertonschool.com>
2021-03-23 04:38:00 -0500

    add 5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free list_t list
+ * @head: linked list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *head2;
+
+	while (*head != NULL)
+	{
+		head2 = *head;
+		*head = head2->next;
+		free(head2);
+	}
+}
