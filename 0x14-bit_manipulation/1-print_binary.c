3fe0f5e7db9cdb0ee79a41e0c1fc76cbdee38057
Santiago Cano <2704@holbertonschool.com>
2021-03-25 20:11:24 -0500

    add 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_binary - convert number to binary and print the result
+ * @n: number
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int n2 = n;
+
+	if (n2 > 0)
+	{
+		n2 = n2 >> 1;
+		print_binary(n2);
+		if (n2 << 1 == n)
+			_putchar('0');
+		else
+			_putchar('1');
+	}
+}
