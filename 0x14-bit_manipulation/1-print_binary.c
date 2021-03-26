63ef43d915d70a4531acb822ca4ffb7177853754
Santiago Cano <2704@holbertonschool.com>
2021-03-25 20:20:00 -0500

    im proud of this

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,10 +8,13 @@ void print_binary(unsigned long int n)
 {
 	unsigned long int n2 = n;
 
+	if (n2 == 0)
+		_putchar('0');
 	if (n2 > 0)
 	{
 		n2 = n2 >> 1;
-		print_binary(n2);
+		if (n2 > 0)
+			print_binary(n2);
 		if (n2 << 1 == n)
 			_putchar('0');
 		else
