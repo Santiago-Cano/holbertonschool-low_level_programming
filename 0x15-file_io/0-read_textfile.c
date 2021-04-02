489830d156018774ba091cd415c744bc76dd18ff
Santiago Cano <2704@holbertonschool.com>
2021-04-01 20:25:18 -0500

    first commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - read a text file and print it to stdout
+ * @filename: name of text file to read
+ * @letters: number of letters it should read and print
+ * Return: Actual number of letters read and printed, otherwise 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t vread, vwrite;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, 0);
+
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+	vread = read(fd, buf, letters);
+	if (vread == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+	vwrite = write(1, buf, vread);
+	close(fd);
+	if (vwrite == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	return (vread);
+}
