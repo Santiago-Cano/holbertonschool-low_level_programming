2867a8436f59caf258ceef5711daa47d4f791381
Santiago Cano <2704@holbertonschool.com>
2021-05-02 20:23:23 -0500

    betty

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 
 /**
- * print_dlistint - print data of doubly linked list
+ * dlistint_len - return num of nodes in doubly linked list
  * @h: head of doubly linked list
  * Return: number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-        size_t nodes = 0;
+	size_t nodes = 0;
 
-        while (h != NULL)
-        {
-                nodes++;
-                h = h->next;
-        }
+	while (h != NULL)
+	{
+		nodes++;
+		h = h->next;
+	}
 
-        return (nodes);
+	return (nodes);
 }
