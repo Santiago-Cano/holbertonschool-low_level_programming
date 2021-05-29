f87078e19c752ad48aac0c9040cef363f03fa391
Santiago Cano <2704@holbertonschool.com>
2021-05-28 23:38:31 -0500

    first commit

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create a hash table of size @size
+ * @size: size of array
+ * Return: pointer to new hash table, NULL on error
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *newtable;
+
+	if (size == 0)
+		return (NULL);
+
+	newtable = malloc(sizeof(hash_table_t));
+	if (!newtable)
+		return (NULL);
+
+	newtable->array = calloc(size, sizeof(hash_node_t **));
+	if (!newtable->array)
+	{
+		free(newtable);
+		return (NULL);
+	}
+	newtable->size = size;
+	return (newtable);
+}
