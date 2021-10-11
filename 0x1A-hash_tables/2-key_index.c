58d53f742d8433ed77d9fe491820d5dfcaba5b81
Santiago Cano <2704@holbertonschool.com>
2021-10-10 19:22:04 -0500

    add task 2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Returns index for a specified key using the hash_djb2 function
+ * @key: Key to find index to be stored in
+ * @size: Size of hash table
+ * Return: Index of specified key
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
